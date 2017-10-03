1fb86bf153ef69701bc8121a5dfacaf409d50b18
Natalia Margolis <nmargolis@hugeinc.com>
2017-10-03 14:58:17 -0700

    Import basic component

---
+++ recording.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Audio} from 'expo';
+
+export default class Recording extends Component {
+  constructor(props){
+    super(props);
+}
+  render () {
+    return (
+    <Text> Start recording </Text>
+    )
+  }
+}
